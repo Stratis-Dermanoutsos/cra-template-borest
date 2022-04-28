8a20143c7feda5a4d5c1a8b35a7e68af4246ab62
Stratis Dermanoutsos <stratis450@gmail.com>
2022-04-28 17:52:57 +0300

    Added basic todo list to HomePage

--- template/src/pages/HomePage.tsx
+++ template/src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import TodoList from 'components/TodoList';
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Container className='mt-5 py-4'>
+            <h1>Home Page</h1>
+            <TodoList />
+        </Container>
     );
 };
 
