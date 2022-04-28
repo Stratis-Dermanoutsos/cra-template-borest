47501090d7609a71da9aec8ea793ae348431c017
Stratis Dermanoutsos <stratis450@gmail.com>
2022-04-28 18:34:54 +0300

    Moved input out of TodoList

--- template/src/components/TodoList.tsx
+++ template/src/components/TodoList.tsx
@@ -1,33 +1,13 @@
-import React, { useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import TodoItem from 'components/TodoItem';
-import {
-    addTodo,
-    selectTodo,
-} from 'features/todoSlice';
+import { selectTodo } from 'features/todoSlice';
 import todo from 'app/models/todo';
 
 const TodoList = () => {
     const todos: todo[] = useSelector(selectTodo);
-    const dispatch = useDispatch();
 
-    const text = useRef<HTMLInputElement | null>(null);
-
-    const handleSubmit = () => {
-        if (!text.current)
-            return;
-
-        dispatch(addTodo(text.current.value));
-        text.current.value = '';
-    }
-
-    return (
-        <>
-            <ul>{todos.map(item => <TodoItem key={item.id} item={item} />)}</ul>
-            <input type='text' ref={text} />
-            <button onClick={handleSubmit}>Add</button>
-        </>
-    );
+    return <ul>{todos.map(item => <TodoItem key={item.id} item={item} />)}</ul>;
 };
 
 export default TodoList;
--- template/src/pages/HomePage.tsx
+++ template/src/pages/HomePage.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useRef } from 'react';
+import { Button, Col, Container, FloatingLabel, Form, Row } from 'react-bootstrap';
 import TodoList from 'components/TodoList';
+import { addTodo } from 'features/todoSlice';
+import { useDispatch } from 'react-redux';
 
 const HomePage = () => {
+    const dispatch = useDispatch();
+    const text = useRef<HTMLInputElement | null>(null);
+
+    const handleSubmit = () => {
+        if (!text.current || !text.current.value)
+            return;
+
+        dispatch(addTodo(text.current.value));
+        text.current.value = '';
+    }
+
     return (
         <Container className='mt-5 py-4'>
             <h1>Home Page</h1>
+            <Row>
+                <Col
+                    xs={{ span: 8, offset: 2 }}
+                    md={{ span: 6, offset: 3 }}
+                >
+                    <FloatingLabel
+                        controlId='floatingInput'
+                        label='I have to ...'
+                        className='mb-3'
+                    >
+                        <Form.Control type='text' ref={text} placeholder='Add todo' />
+                    </FloatingLabel>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='d-flex justify-content-center'>
+                    <Button size='lg' variant='primary' onClick={handleSubmit}>Add todo</Button>
+                </Col>
+            </Row>
             <TodoList />
         </Container>
     );
